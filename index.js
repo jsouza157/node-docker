eeff917914d888f22f25094a0a8df0b4d6d8c4e6
Jefferson <jeffersondasilva1@hotmail.com>
2021-10-02 14:29:08 -0300

    add dotenv

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
+require('dotenv').config()
 const express = require("express");
 
 const app = express();
 
-const PORT = 3000
-const HOST = '0.0.0.0'
+const PORT = process.env.PORT | 3000
+const HOST = process.env.HOST | '0.0.0.0'
 
 app.get("/", (req, res) => {
-  res.send("Hi Docker azul!!!");
+  res.send("Hi Dockers!!!");
 });
 
 app.listen(PORT, HOST);
